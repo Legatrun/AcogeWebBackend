79f646d5102ebaa32ea5cfe97a0e938c08c9e10f
rpaz <roberto.paz@alphasys.com.bo>
2021-03-23 17:35:28 -0400

    Empresa Controller

---
+++ proyecto/Controllers/EmpresaController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using proyecto.Models;
+
+namespace proyecto.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    [RoutePrefix("api/Empresa")]
+    public class EmpresaController : ApiController
+    {
+        EmpresaDataAccess objEmpresa = new EmpresaDataAccess();
+
+        [HttpPost]
+        [Route("Consultar")]
+        public Empresa Consultar()
+        {
+            return objEmpresa.ConsultarEmpresa();
+        }
+      
+    }
+}
